afefb877321a79b3708bcf1e32fde2aead8969b6
Anastasia-Demidova <60142958+Anastasia-Demidova@users.noreply.github.com>
2020-04-03 03:37:29 +0300

    Implementation app "Predictions"

--- AppWatchOS WatchKit Extension/InterfaceController.swift
+++ AppWatchOS WatchKit Extension/InterfaceController.swift
@@ -9,23 +9,13 @@
 import WatchKit
 import Foundation
 
-
 class InterfaceController: WKInterfaceController {
-
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
+    @IBOutlet weak var textLabel: WKInterfaceLabel!
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    let array = ["YES!","NO!","Ask me later","MayBe","DON'T ASK ME!"]
+    
+    @IBAction func actionButtonTapped() {
+        textLabel.setText(array[Int.random(in: 0...array.count - 1)])
     }
-
 }
